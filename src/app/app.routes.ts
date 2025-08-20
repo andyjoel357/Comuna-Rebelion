cceaa1d925ae7efa5aea4cedca2cdc9e1d98567c
andyjoel357 <joelandy357@gmail.com>
2025-08-20 12:45:27 -0500

    baseHref

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,10 @@ import { NosotrosComponent } from './home/nosotros/nosotros.component';
 import { UneteComponent } from './home/unete/unete.component';
 import { Error404Component } from './home/error404/error404.component';
 
-
 export const routes: Routes = [
-  {path:'inicio', component:InicioComponent },
-  {path:'nosotros', component:NosotrosComponent},
-  {path:'unete', component:UneteComponent},
-
-  {path:'', redirectTo:'inicio', pathMatch:'full'},
-  {path:'**', component:Error404Component}
+  { path: 'inicio', component: InicioComponent },  
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'unete', component: UneteComponent },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: '**', component: Error404Component }
 ];
